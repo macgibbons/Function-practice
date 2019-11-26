50bf79d66717197e6b3c2043d6f75f41b68d1977
macgibbons <macgibbons@gmail.com>
2019-11-26 11:18:10 -0600

    finished the practice functions

--- practice.js
+++ practice.js
@@ -142,3 +142,209 @@ console.log(svenFunction(0))
 // Is your function a pure function? Why or why not? If you're unsure, talk to your teammates or your instructors.
 // What happens if you multiply random() by 0?
 
+
+
+
+
+
+
+// Practice: Fast Food (Multiple Parameters)
+// Your job in this exercise is to write a function that returns an object that represents a fast food meal. The customer will be able to order a sandwich, a side item (like fries or apples), a drink, and a dessert. Your function should take all of those externally defined values as input (i.e. parameters) and then return an object like this.
+
+// {
+//     "sandwich": "Ultimate Slammer",
+//     "side": "Potato wedges",
+//     "drink": "Mr. Pepper",
+//     "dessert": "Fudge sundae"
+// }
+
+const buildMeal = (sandwich, side, drink, dessert) => {
+    const meal = {
+        "sandwich": sandwich,
+        "side": side,
+        "drink": drink,
+        "dessert": dessert
+    };
+return meal;
+}
+const myMeal = buildMeal ('Ultimate Slammer', 'potato wedges', 'mr. pepper', 'fudge sundae',)
+console.log(myMeal);
+
+// Tip: Since each of these values are separate items, then there needs to be one parameter for each one. Then you need to specify four arguments.
+
+// const takeOutBag = orderMeal("Ultimate Slammer", "Fudge sundae", "Mr. Pepper", "Potato wedges")
+// Exploration Tasks
+// What happens if you define more parameters than arguments?
+// What happens if you change the order of your parameters?
+// What happens if you change the order of your argument values when you invoke the function?
+// What happens if you remove one of your parameters?
+
+
+
+
+// Practice: Same Chores, Different Days
+// Here's a fancy term for you: Higher Order Functions. It's a function that takes another function as a parameter and uses it for its own purposes.
+
+// Here's a simple example. This is a function that reads in a number, then returns that number multiplied by 2.
+
+// const twice = number => number * 2
+
+// twiceTwo = twice(2)
+// console.log(twiceTwo)  // 4
+
+// twiceSix = twice(6)
+// console.log(twiceSix)  // 12
+
+// twiceThirteen = twice(13)
+// console.log(twiceThirteen)  // 26
+// You can make functions that multiply numbers by any amount. Make two more functions: one that multiplies by 5, and one that squares a number.
+
+// const timesFive = number => number * 5
+// console.log(timesFive(2))  // 10
+// console.log(timesFive(5))  // 25
+
+// const square = number => number * number
+// console.log(square(4))  // 16
+// console.log(square(13))  // 169
+// So now you have three functions, each of which does something different to a number.
+
+// const twice = number => number * 2
+// const timesFive = number => number * 5
+// const square = number => number * number
+// Now you can define another function that takes one of those functions as input. It's a more generic function that will perform any task on a number, but needs you to tell it what it should do.
+
+// /*
+//     I can make any number bigger, but you need to tell
+//     me how much bigger by giving me the function you
+//     want me to invoke on the number
+// */
+ const bigify = (number, operationFunction) => {
+     const result = operationFunction(number)
+     return result
+ }
+//  Now take the same number and perform all three operations on it by invoking bigify().
+
+const number = 7
+// const by2 = bigify(number, twice)  // 14
+// const by5 = bigify(number, timesFive)  // 35
+// const byItself = bigify(number, square)  // 49
+// Lightning Exercise: Define another function that cubes a number. Then augment the code above by passing the function to bigify()
+
+const cubed = (number) => number * number * number;
+
+console.log(cubed(2));
+
+const byCubed = bigify(number, cubed);
+
+console.log(byCubed); //343 
+
+
+
+// You are now going to do something similiar. You are going to make the same person perform a series of three chores. Then you get to decide which three chores, depending on the day.
+
+
+
+
+
+
+// Defining the Chores
+// First define six (6) functions that defines a single parameter whose value should be an object. That object will look like this.
+
+// {
+//     firstName: "Donald",
+//     lastName: "McLelland"
+// }
+// Each function should return a string in the following format, if the object parameter represents Donald McLelland.
+
+// "Donald McLelland went to the grocery store"
+// or
+
+// "Donald McLelland got the engine oil changed"
+// So think of six random tasks that you perform on a regular basis and define a function for each one. We will start you off with a hint.
+
+// const xxxxx = person => `${} ${} `
+const mac = {
+    firstName: "Mac",
+    lastName: "Gibbons"
+}
+
+const gotGas = (person) => `${person.firstName} ${person.lastName} filled up his tank`;
+const whatIsYourName = (person) => `${person.firstName}'s last name is ${person.lastName}.`
+const wentForaRun = person => `${person.firstName} went for a jog today but had to stop because it rained`
+
+console.log(gotGas(mac));
+console.log(whatIsYourName(mac));
+console.log(wentForaRun(mac));
+
+
+
+
+
+// Lazy Day
+// Now define a function that has three parameters. The purpose of each parameter is as follows:
+
+// chore - A function that will perform a chore
+// person - An object representing a perform. It should have a firstName and a lastName property.
+// day - The weekday to perform the task (e.g. "Tuesday")
+// We suggest you name this higher order function dayPlanner().
+
+// The return value of dayPlanner should be a string in the following format, assuming the person object represents Yolanda Johnson, the chore was mowing the lawn, and the day was Thursday.
+
+// "Yolanda Johnson mowed the lawn on Thursday."
+// Log that output to the developer console.
+const theChore = () => {
+    return 'took the trash out'
+}
+const thePerson = {
+    firstName: 'Mac',
+    lastName: 'Gibbons'
+}
+const dayOfWeek = 'Tuesday'
+
+const dayPlanner = (chore, person, day) => {
+   return `${person.firstName} ${person.lastName} ${chore} on ${dayOfWeek}`
+}
+
+
+console.log(dayPlanner(theChore(), thePerson, dayOfWeek))
+
+
+
+
+
+
+// Busy Day
+// Refactor the dayPlanner higher order function to accept five (5) parameters. The first three should be functions. The fourth should be the person object. The fifth should be the day string.
+
+// (firstChore, secondChore, thirdChore, person, day)
+// Inside the higher order function, invoke all three chore functions and format the return value for dayPlanner to follow this format.
+
+// "On Monday, Yolanda Johnson mowed the lawn, and Yolanda Johnson
+// picked up the dry cleaning, and Yolanda Johnson replaced the
+// batteries in the smoke detectors."
+// Log that output to the developer console.
+
+const firstChore = () => {
+    return `folded the laundry`
+}
+
+const secondChore = () => {
+    return `went grocery shopping`
+}
+
+const thirdChore = () => {
+    return `meal prepped for the week`
+}
+
+const theNewPerson = {
+    firstName: "Morgan",
+    lastName: "Bonham"
+}
+
+const theDayOfTheWeek = "Sunday"
+
+const newDayPlanner = (firstChore,secondChore, thirdChore, person, day) => {
+    return `On ${day}, ${person.firstName} ${person.lastName} ${firstChore}, ${secondChore}, and ${thirdChore} `
+}
+
+console.log(newDayPlanner(firstChore(), secondChore(), thirdChore(), theNewPerson, theDayOfTheWeek));
